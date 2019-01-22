4c5a1ad0d35cae8e9dac92cd8634108bb2802705
eldertugboat786 <padri95@gmail.com>
2019-01-23 00:11:53 +0100

    Create simple first layout

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,31 @@
+<template lang="html">
+  <aside class="menu">
+    <p class="menu-label">
+      General
+    </p>
+    <ul class="menu-list">
+      <li><a>Dashboard</a></li>
+      <li><a>Customers</a></li>
+    </ul>
+    <p class="menu-label">
+      Categories
+    </p>
+    <ul class="menu-list">
+      <li><a>List</a></li>
+      <li><a>Create</a></li>
+    </ul>
+  </aside>
+</template>
+
+<script>
+
+export default {
+  name: 'Sidebar'
+}
+</script>
+
+<style lang="css" scoped>
+.menu {
+  margin-left: 15px
+}
+</style>
---
+++ src/views/Btest.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="about">
+    <NavBar />
+    <div class="columns">
+      <div class="column is-one-quarter">
+        <Sidebar />
+      </div>
+      <div class="column">
+        <div class="columns is-multiline">
+          <div class="column is-one-third-desktop is-half-tablet">
+            <Note />
+          </div>
+          <div class="column is-one-third-desktop is-half-tablet">
+            <Note />
+          </div>
+          <div class="column is-one-third-desktop is-half-tablet">
+            <Note />
+          </div>
+          <div class="column is-one-third-desktop is-half-tablet">
+            <Note />
+          </div>
+          <div class="column is-one-third-desktop is-half-tablet">
+            <Note />
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</template>
+
+
+<script>
+// @ is an alias to /src
+import NavBar from '@/components/NavBar.vue'
+import Sidebar from '@/components/Sidebar.vue'
+import Note from '@/components/Note.vue'
+
+export default {
+  name: 'homebtest',
+  components: {
+    NavBar,
+    Sidebar,
+    Note
+  }
+}
+</script>
